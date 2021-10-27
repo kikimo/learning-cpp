41b30c85535f4e8b1f21fadfbacabca3a3b6e58d
kikimo <451943+kikimo@users.noreply.github.com>
2021-10-27 15:42:12 +0800

    add gtest demo

---
+++ gtest-demo/main.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+// Google Test test cases are created using a C++ preprocessor macro
+// Here, a "test suite" name and a specific "test name" are provided.
+TEST(hello_module, hello_test) {
+    std::cout << "Hello world!" << std::endl;
+    // Google Test will also provide macros for assertions.
+    ASSERT_EQ(1+1, 2);
+}
+
+// Google Test can be run manually from the main() function
+// or, it can be linked to the gtest_main library for an already
+// set-up main() function primed to accept Google Test test cases.
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
+// Build command: g++ -std=c++14 main.cpp -lgtest
